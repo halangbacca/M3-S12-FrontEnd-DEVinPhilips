86e534fdc306113da6bd3fb1f998032e719aae4e
Rodolfo Bortolozo <rodolfobortolozo@yahoo.com.br>
2023-07-17 09:18:42 -0300

    acrescentado o cadatro de empresa no menu

--- src/app/features/customization/customization-routing.module.ts
+++ src/app/features/customization/customization-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddEditCompanyComponent } from './add-edit-company/add-edit-company.component';
+import { LayoutComponent } from 'src/app/shared/layout/layout.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AddEditCompanyComponent,
+    component: LayoutComponent,
+    children: [
+      { path: '', component: AddEditCompanyComponent },
+    ]
   },
 ];
 @NgModule({
