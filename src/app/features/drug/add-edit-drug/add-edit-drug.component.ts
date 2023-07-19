24f2c8dc50a3e6f5858d97b665e81656f34527a6
halangbacca <halanbacca@outlook.com>
2023-07-19 17:10:46 -0300

    Correções gerais da tela de cadastro de medicamentos

--- src/app/features/drug/add-edit-drug/add-edit-drug.component.ts
+++ src/app/features/drug/add-edit-drug/add-edit-drug.component.ts
@@ -1,3 +1,4 @@
+import { formatDate } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Drug } from 'src/app/shared/models/Drug';
@@ -13,16 +14,16 @@ import { PatientService } from 'src/app/shared/services/patient/patient.service'
 })
 export class AddEditDrugComponent {
   drug = {} as Drug;
-
   patient = {} as Patient;
 
   formDrug!: FormGroup;
+  formPatient!: FormGroup;
 
   drugs = [] as Drug[];
-
   pacientes = [] as Patient[];
 
   isDisabled = true;
+  isEditing = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -34,9 +35,8 @@ export class AddEditDrugComponent {
   createform(drug: Drug) {
     this.formDrug = this.formBuilder.group({
       id: [drug.id],
-      medicamento: [drug.nome],
       idPaciente: [drug.idPaciente],
-      nomePaciente: [drug.nomePaciente],
+      nomePaciente: [drug.nomePaciente, [Validators.required]],
       nome: [
         drug.nome,
         [
@@ -50,54 +50,88 @@ export class AddEditDrugComponent {
       tipo: [drug.tipo, [Validators.required]],
       quantidade: [drug.quantidade, [Validators.required]],
       unidade: [drug.unidade, [Validators.required]],
-      observacoes: [drug.observacoes, [Validators.required]],
+      observacoes: [
+        drug.observacoes,
+        [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(1000),
+        ],
+      ],
+      statusDoSistema: [true],
+    });
+  }
+
+  createPatientForm() {
+    this.formPatient = this.formBuilder.group({
+      nomePaciente: ['', [Validators.required]],
+      medicamento: [''],
     });
   }
 
   ngOnInit(): void {
     this.createform(this.drug);
+    this.createPatientForm();
 
     this.patientService.getAllPatient().subscribe((ret) => {
       this.pacientes = ret;
     });
 
-    this.formDrug.get('data')?.setValue(new Date(Date.now()).toLocaleString());
+    this.formDrug
+      .get('data')
+      ?.setValue(formatDate(new Date(), 'dd-MM-yyyy', 'en'));
 
     this.formDrug
       .get('horario')
-      ?.setValue(new Date(Date.now()).toLocaleString());
+      ?.setValue(formatDate(new Date(), 'H:mm:ss', 'en'));
   }
 
   onFocus() {
-    this.patientService.getAllPatient().subscribe((ret) => {
-      this.pacientes = ret;
-    });
+    if (this.formPatient.get('nomePaciente')?.value != null) {
+      this.patientService.getAllPatient().subscribe((ret) => {
+        this.pacientes = ret;
 
-    this.drugService
-      .getDrugByPatientName(this.formDrug.get('nomePaciente')?.value)
-      .subscribe((ret) => {
-        this.drugs = ret;
+        this.drugService
+          .getDrugByPatientName(this.formPatient.get('nomePaciente')?.value)
+          .subscribe((ret) => {
+            this.drugs = ret;
+          });
       });
+    }
 
     this.pacientes.forEach((patient) => {
-      if (patient.nome === this.formDrug.get('nomePaciente')?.value) {
+      if (patient.nome === this.formPatient.get('nomePaciente')?.value) {
         this.formDrug.get('idPaciente')?.setValue(patient.id);
+        this.formDrug.get('nomePaciente')?.setValue(patient.nome);
       }
     });
 
-    if (this.formDrug.get('medicamento')?.value != null) {
+    if (this.formPatient.get('medicamento')?.value != null) {
       this.drugs.forEach((item) => {
-        if (item.nome === this.formDrug.get('medicamento')?.value) {
+        if (item.nome === this.formPatient.get('medicamento')?.value) {
           this.formDrug.patchValue(item);
+          this.isDisabled = false;
+          this.isEditing = true;
         }
-        this.isDisabled = false;
       });
     }
   }
 
   clearForm() {
     this.formDrug.reset();
+    this.formPatient.reset();
     this.drug = {} as Drug;
+
+    this.isDisabled = true;
+    this.isEditing = false;
+
+    this.formDrug
+      .get('data')
+      ?.setValue(formatDate(new Date(), 'dd-MM-yyyy', 'en'));
+
+    this.formDrug
+      .get('horario')
+      ?.setValue(formatDate(new Date(), 'H:mm:ss', 'en'));
   }
 
   saveDrug(drug: Drug) {
