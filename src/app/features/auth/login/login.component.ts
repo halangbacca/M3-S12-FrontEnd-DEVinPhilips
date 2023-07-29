ad52c9e73ba1f03902992bdbee60ec7f12b38672
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2023-07-29 01:34:15 -0300

    Altera layout da página de login

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,22 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Login } from 'src/app/shared/models/Login';
 import { AuthService } from 'src/app/shared/services/auth/auth.service';
-import { NotificationService } from 'src/app/shared/services/notification.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+
+export class LoginComponent implements OnInit {
   form!: FormGroup;
   login: Login = {};
-  
+
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthService
-  ) {
+  ) { }
+
+  ngOnInit() {
     this.form = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       senha: ['', [Validators.required, Validators.minLength(3)]],
@@ -25,9 +27,7 @@ export class LoginComponent {
 
   onSubmit() {
     if (this.form.valid) {
-      this.login.email = this.form.get('email')?.value;
-      this.login.senha = this.form.get('senha')?.value;
-
+      this.login = {...this.form.value};
       this.authService.login(this.login);
     } else {
       Object.keys(this.form.controls).forEach((field) => {
